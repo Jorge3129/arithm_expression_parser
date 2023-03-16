d0fe6bba0532d7d4d70338b5444c296532ddb6eb
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-03-16 10:52:28 +0200

    Java Parser - User Input

--- java_prolog_parser/src/main/java/com/sanchenko/Main.java
+++ java_prolog_parser/src/main/java/com/sanchenko/Main.java
@@ -1,8 +1,26 @@
 package com.sanchenko;
 
 
+import com.sanchenko.exp.Exp;
+import com.sanchenko.imperative_parser.ImperativeParser;
+
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) throws Exception {
-
+      Scanner input = new Scanner(System.in);
+      while (true) {
+         System.out.print("expr>");
+         String line = input.nextLine();
+         if (":q".equals(line)) {
+            break;
+         }
+         try {
+            Exp tree = ImperativeParser.parse(line);
+            System.out.println(tree);
+         } catch (Exception e) {
+            System.out.println(e);
+         }
+      }
    }
 }
